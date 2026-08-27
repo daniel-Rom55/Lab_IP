efbe45b9cc4a16059583d9b3188b8f136dc60d0e
Hector Roman <Hector Roman@example.com>
2026-08-27 10:41:57 -0600

    create primo.py

---
+++ UNIDAD 1/01_EJERCICIOS1/suma.py
@@ -0,0 +1,7 @@
+num1=2
+num2=3
+num3=4
+res=num1+num2*num3
+print(res)
+
+
---
+++ UNIDAD 1/UNIDAD1_PRACTICA_FINAL/primo.py
@@ -0,0 +1,11 @@
+n = int(input("Introduce un numero: "))
+if n<=1:
+    print("El numero no es primo")
+i=2
+while i<n:
+    if n%i==0:
+        print("El numero no es primo")
+        break
+    i+=1
+else:
+    print("El numero es primo")
